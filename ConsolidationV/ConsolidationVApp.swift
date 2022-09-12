8acd3b7207bdb16ed7cf2e87953b4c4a156b738a
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-09-12 14:38:19 -0700

    First commit

---
+++ ConsolidationV/ConsolidationVApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConsolidationVApp.swift
+//  ConsolidationV
+//
+//  Created by Vincent Salinas on 9/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ConsolidationVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
